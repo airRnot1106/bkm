4c86bcb754fb6e066e9fcb072c3e846a4ce5de8e
airRnot1106 <yngw.rintaro@gmail.com>
2025-07-19 23:01:02 +0900

    test: add bookamrk-id test

---
+++ src/core/bookmark/bookmark.test.ts
@@ -0,0 +1,7 @@
+Deno.test("bookmark-id", (t) => {
+  t.step("should create a bookmark id", (t) => {
+    t.step("should accept valid bookmark id format", () => {});
+    t.step("should reject invalid bookmark id format", () => {});
+    t.step("should be equal when ids are the same", () => {});
+  });
+});
