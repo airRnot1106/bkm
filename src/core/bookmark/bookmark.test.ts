d9980325011139dc0b02b7ad47fae804f5281b37
airRnot1106 <yngw.rintaro@gmail.com>
2025-07-20 00:59:37 +0900

    test: add bookmark-url test

--- src/core/bookmark/bookmark.test.ts
+++ src/core/bookmark/bookmark.test.ts
@@ -2,7 +2,7 @@ import * as fc from "fast-check";
 import { assertEquals } from "@std/assert";
 import { Result } from "@praha/byethrow";
 import { bookmarkGenerator } from "./bookmark.mock.ts";
-import { BookmarkId, BookmarkTitle } from "./bookmark.ts";
+import { BookmarkId, BookmarkTitle, BookmarkUrl } from "./bookmark.ts";
 
 Deno.test("bookmark-id", async (t) => {
   await t.step("should create a bookmark id", async (t) => {
@@ -70,6 +70,47 @@ Deno.test("bookmark-title", async (t) => {
           (bookmarkTitle) => {
             const result1 = Result.parse(BookmarkTitle, bookmarkTitle);
             const result2 = Result.parse(BookmarkTitle, bookmarkTitle);
+            if (Result.isSuccess(result1) && Result.isSuccess(result2)) {
+              assertEquals(result1.value, result2.value);
+            }
+          },
+        ),
+      );
+    });
+  });
+});
+
+Deno.test("bookmark-url", async (t) => {
+  await t.step("should create a bookmark url", async (t) => {
+    await t.step("should accept valid bookmark url format", () => {
+      fc.assert(
+        fc.property(
+          bookmarkGenerator.bookmarkUrl.valid(),
+          (maybeBookmarkUrl) => {
+            const result = Result.parse(BookmarkUrl, maybeBookmarkUrl);
+            assertEquals(Result.isSuccess(result), true);
+          },
+        ),
+      );
+    });
+    await t.step("should reject invalid bookmark url format", () => {
+      fc.assert(
+        fc.property(
+          bookmarkGenerator.bookmarkUrl.invalid(),
+          (maybeBookmarkUrl) => {
+            const result = Result.parse(BookmarkUrl, maybeBookmarkUrl);
+            assertEquals(Result.isFailure(result), true);
+          },
+        ),
+      );
+    });
+    await t.step("should be equal when titles are the same", () => {
+      fc.assert(
+        fc.property(
+          bookmarkGenerator.bookmarkUrl.valid(),
+          (bookmarkUrl) => {
+            const result1 = Result.parse(BookmarkUrl, bookmarkUrl);
+            const result2 = Result.parse(BookmarkUrl, bookmarkUrl);
             if (Result.isSuccess(result1) && Result.isSuccess(result2)) {
               assertEquals(result1.value, result2.value);
             }
