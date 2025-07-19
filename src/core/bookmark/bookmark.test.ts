48577dc1fbb8ec8d7eccad49b41c3e70a50a80b8
airRnot1106 <yngw.rintaro@gmail.com>
2025-07-19 23:57:39 +0900

    test: add bookmark id test

--- src/core/bookmark/bookmark.test.ts
+++ src/core/bookmark/bookmark.test.ts
@@ -1,7 +1,40 @@
-Deno.test("bookmark-id", (t) => {
-  t.step("should create a bookmark id", (t) => {
-    t.step("should accept valid bookmark id format", () => {});
-    t.step("should reject invalid bookmark id format", () => {});
-    t.step("should be equal when ids are the same", () => {});
+import * as fc from "fast-check";
+import { assertEquals } from "@std/assert";
+import { Result } from "@praha/byethrow";
+import { bookmarkGenerator } from "./bookmark.mock.ts";
+import { BookmarkId } from "./bookmark.ts";
+
+Deno.test("bookmark-id", async (t) => {
+  await t.step("should create a bookmark id", async (t) => {
+    await t.step("should accept valid bookmark id format", () => {
+      fc.assert(
+        fc.property(bookmarkGenerator.bookmarkId.valid(), (maybeBookmarkId) => {
+          const result = Result.parse(BookmarkId, maybeBookmarkId);
+          assertEquals(Result.isSuccess(result), true);
+        }),
+      );
+    });
+    await t.step("should reject invalid bookmark id format", () => {
+      fc.assert(
+        fc.property(
+          bookmarkGenerator.bookmarkId.invalid(),
+          (maybeBookmarkId) => {
+            const result = Result.parse(BookmarkId, maybeBookmarkId);
+            assertEquals(Result.isFailure(result), true);
+          },
+        ),
+      );
+    });
+    await t.step("should be equal when ids are the same", () => {
+      fc.assert(
+        fc.property(bookmarkGenerator.bookmarkId.valid(), (bookmarkId) => {
+          const result1 = Result.parse(BookmarkId, bookmarkId);
+          const result2 = Result.parse(BookmarkId, bookmarkId);
+          if (Result.isSuccess(result1) && Result.isSuccess(result2)) {
+            assertEquals(result1.value, result2.value);
+          }
+        }),
+      );
+    });
   });
 });
