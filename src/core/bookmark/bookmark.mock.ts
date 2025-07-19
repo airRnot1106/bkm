40387da67d347ede2f47a0d695871992c6b0ad1a
airRnot1106 <yngw.rintaro@gmail.com>
2025-07-20 00:42:54 +0900

    test: add bookmark-title generator

--- src/core/bookmark/bookmark.mock.ts
+++ src/core/bookmark/bookmark.mock.ts
@@ -1,4 +1,7 @@
 import * as fc from "fast-check";
+import { BOOKMARK_TITLE_MAX_LENGTH } from "./bookmark.ts";
+
+const MAX_NAT = 10000;
 
 export const bookmarkGenerator = {
   bookmarkId: {
@@ -7,6 +10,38 @@ export const bookmarkGenerator = {
       fc.string().filter((id) =>
         !/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
           .test(id)
+      ),
+  },
+  bookmarkTitle: {
+    valid: () =>
+      fc.oneof(
+        fc.string().map((title) => title.trim()).filter((title) =>
+          title.length > 0 && title.length <= BOOKMARK_TITLE_MAX_LENGTH
+        ),
+        fc.tuple(
+          fc.nat({ max: MAX_NAT }),
+          fc.nat({ max: MAX_NAT }),
+        ).map((
+          [start, end],
+        ) =>
+          " ".repeat(start) + "X".repeat(BOOKMARK_TITLE_MAX_LENGTH) +
+          " ".repeat(end)
+        ),
+      ),
+    invalid: () =>
+      fc.oneof(
+        fc.string().filter((title) =>
+          title.length < 1 || title.length > BOOKMARK_TITLE_MAX_LENGTH
+        ),
+        fc.tuple(
+          fc.nat({ max: MAX_NAT }),
+          fc.nat({ max: MAX_NAT }),
+        ).map(([start, end]) =>
+          " ".repeat(start) + "X".repeat(BOOKMARK_TITLE_MAX_LENGTH + 1) +
+          " ".repeat(end)
+        ),
+        fc.constant(""),
+        fc.nat({ max: MAX_NAT }).map((num) => " ".repeat(num)),
       ),
   },
 };
