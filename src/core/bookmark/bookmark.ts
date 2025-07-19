915e9aad1c32168f1653f6bd3ce4da3622962c0c
airRnot1106 <yngw.rintaro@gmail.com>
2025-07-20 00:42:41 +0900

    feat: add BookmarkTitle

--- src/core/bookmark/bookmark.ts
+++ src/core/bookmark/bookmark.ts
@@ -5,3 +5,15 @@ export declare const BookmarkIdBrand: unique symbol;
 export const BookmarkId = z.uuidv4().brand<typeof BookmarkIdBrand>();
 
 export type BookmarkId = z.infer<typeof BookmarkId>;
+
+export declare const BookmarkTitleBrand: unique symbol;
+
+export const BOOKMARK_TITLE_MAX_LENGTH = 50;
+
+export const BookmarkTitle = z.string().trim().min(1).max(
+  BOOKMARK_TITLE_MAX_LENGTH,
+).brand<
+  typeof BookmarkTitleBrand
+>();
+
+export type BookmarkTitle = z.infer<typeof BookmarkTitle>;
