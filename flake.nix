143fdbafade02177043591dcea93f0e491b580d5
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-12 14:28:42 +0900

    chore: init flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      systems,
+      ...
+    }:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            cobra-cli
+            go
+            gopls
+            gotest
+          ];
+        };
+      });
+    };
+}
