ca9a75aecaadf219be3c52ae27dabfd79611a6be
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-12 15:01:24 +0900

    chore: init treefmt and pre-commit

--- flake.nix
+++ flake.nix
@@ -1,28 +1,81 @@
 {
   inputs = {
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
+      self,
+      git-hooks,
       nixpkgs,
       systems,
+      treefmt-nix,
       ...
     }:
     let
-      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
+          f {
+            inherit system;
+            pkgs = nixpkgs.legacyPackages.${system};
+          }
+        );
     in
     {
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            cobra-cli
-            go
-            gopls
-            gotest
-          ];
-        };
-      });
+      devShells = eachSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+
+            packages = with pkgs; [
+              cobra-cli
+              go
+              golangci-lint
+              gopls
+              gotest
+            ];
+          };
+        }
+      );
+      formatter = eachSystem (
+        { pkgs, ... }:
+        treefmt-nix.lib.mkWrapper pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            gofmt.enable = true;
+            nixfmt.enable = true;
+          };
+        }
+      );
+      checks = eachSystem (
+        { pkgs, system, ... }:
+        {
+          pre-commit-check = git-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              golangci-lint = {
+                enable = true;
+                extraPackages = with pkgs; [ go ];
+              };
+              treefmt = {
+                enable = true;
+                package = self.formatter.${system};
+              };
+            };
+          };
+        }
+      );
     };
 }
